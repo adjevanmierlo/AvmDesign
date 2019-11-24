2faf72e176b171903f3eabb02bde2a094f0a8972
adjevanmierlo <adjevanmierlo@MacBook-Pro-van-adjevanmierlo.local>
2019-11-24 21:41:32 +0100

    menu active, php helper, underline styling hover

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
 <div class="col landing">
     <h1>Hey, ik ben <mark class="name">Adje van Mierlo.</mark></h1>
-    <p><span class="mark">&#60;</span> een developer klaar voor een nieuwe uitdaging <span class="mark">&#62;</span></p>
+    <p><span class="mark">&#60;</span> developer klaar voor een nieuwe uitdaging <span class="mark">&#62;</span></p>
     <a href="{{ url('/#section2') }}" class="btn btn-outline-primary">Wie ben ik.
     </a>
   </div>
--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -5,15 +5,14 @@
 </button>
 <div class="collapse navbar-collapse" id="collapsibleNavbar">
   <ul class="navbar-nav ml-auto">
-    <li class="nav-item">
-      <a class="nav-link" href=#">Home</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Wie ben ik</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Contact</a>
-    </li>
+      <li class="nav-item {{ set_active ('/')}}">
+        <a class="nav-link scroll" href="{{ url('/') }}">home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item {{ set_active ('About')}}">
+        <a class="nav-link scroll" href="{{ url('/') }}">about</a>
+      </li>
+      <li class="nav-item {{ set_active ('Contact')}}">
+        <a class="nav-link scroll" href="{{ url('/') }}">contact</a>
   </ul>
 </div>
 </nav>
